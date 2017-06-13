4642c0c1a8bc9fb1b806962007c5ef3038140527
Murph Grainger <murph.grainger@gmail.com>
2017-06-13 09:41:23 -0600

    responsive cards

--- src/components/our-team/OurTeam.js
+++ src/components/our-team/OurTeam.js
@@ -14,29 +14,29 @@ class OurTeam extends React.Component {
               <h2 className='headline'>The Galvanize Advantage</h2>
             </div>
             <ProfileGrid />
-            <Row>
-              <Col xs="12" sm="6" md="3">
+            <Row className="card-row">
+              <Col className="card-col" xs="12" sm="6" md="6" lg="3">
               <Card block className="text-center">
                 <CardTitle>Instructors</CardTitle>
                 <CardText className="description">Teaching web development requires unique people dedicated to their students learning.  We have them.</CardText>
                 <Button>See Individual Profiles</Button>
               </Card>
               </Col>
-              <Col xs="12" sm="6" md="3">
+              <Col className="card-col" xs="12" sm="6" md="6" lg="3">
               <Card block className="text-center">
                 <CardTitle>Career Services</CardTitle>
                 <CardText className="description">We prepare students to leverage their new technical skills into a career as a full-stack developer.</CardText>
                 <Button>See the Team</Button>
               </Card>
               </Col>
-              <Col xs="12" sm="6" md="3">
+              <Col className="card-col" xs="12" sm="6" md="6" lg="3">
               <Card block className="text-center">
                 <CardTitle>Student Success</CardTitle>
                 <CardText className="description">Our Student Success team ensures your experience as a Galvanize student is everything you imagined it could be.</CardText>
                 <Button>See Profile</Button>
               </Card>
               </Col>
-              <Col xs="12" sm="6" md="3">
+              <Col className="card-col" xs="12" sm="6" md="6" lg="3">
               <Card block className="text-center">
                 <CardTitle>Entrepreneurs</CardTitle>
                 <CardText className="description">Learn and network surrounded by startup companies in an array of industries.</CardText>
