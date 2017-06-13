48c8fc4957d91aa50d5b537766c8cd9a60d14f11
Murph Grainger <murph.grainger@gmail.com>
2017-06-12 19:42:15 -0600

    page title built

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -10,8 +10,10 @@ class HomePage extends React.Component {
         return (
           <div>
             <div className='hero-section'>
-              <Col sm='8' md='6' className="grid-cell">
-              <h2>Web Development Immersive</h2>
+              <Col sm='9' md='6' className="grid-cell">
+                <div className='page-title'>
+                  <h4>Web Development Immersive</h4>
+                </div>
               <h1>Become a Web Developer</h1>
               <p>Our 24-week Web Development Immersive Program will transform you from a beginner coder into a job-ready full-stack web developer.</p>
               <p><Button color="primary">Start Your Application</Button></p>
