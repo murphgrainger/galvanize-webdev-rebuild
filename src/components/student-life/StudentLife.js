f2acc3ec1b951878ae4aa6e41e118b8aecb634d1
Murph Grainger <murph.grainger@gmail.com>
2017-06-13 13:16:14 -0600

    added arrow and text

--- src/components/student-life/StudentLife.js
+++ src/components/student-life/StudentLife.js
@@ -14,7 +14,14 @@ class StudentLife extends React.Component {
                 <h2 className='headline-parallax'>Student Life</h2>
               </div>
             </div>
+            <Col sm='12' className="lead-text-column">
+              <p className="lead">Our curriculum is built internally and consistently updated along with the quickly changing tech scene.  While each day is unique, the core of our program is morning lectures, followed by personalized learning time where students work through exercises at their own pace to master content objectives.</p>
+            </Col>
             <Col sm='12' className="timeline-col">
+              <Row className="callout-header">
+                <h3 className="callout">
+                  <img className="arrow" src={require("../../images/arrow.png")}/> &nbsp;Daily Plan</h3>
+              </Row>
               <Timeline/>
             </Col>
             </div>
