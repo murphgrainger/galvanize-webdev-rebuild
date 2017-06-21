111245000a1d0479886f8eb9b3f2b606635768e0
Murph Grainger <murph.grainger@gmail.com>
2017-06-21 15:47:29 -0600

    added links to alumni buttons

--- src/components/alumni/AlumniCard.js
+++ src/components/alumni/AlumniCard.js
@@ -18,6 +18,10 @@ class Alumni extends React.Component {
                     <p className="subtitle">{this.props.item.currentCompany} | Denver, CO</p>
                     <p className="subtitle">{this.props.item.cohort}</p>
                     <p className="story">{this.props.item.story}</p>
+                    <div>
+                      <a href={'mailto:' + this.props.item.email}>   <Button className="contact-me-button">Contact Me</Button></a>
+                    <a href={this.props.item.github} target="_blank"> <Button className="contact-me-button">See My Projects</Button></a>
+                    </div>
                   </div>
                 </Card>
               </Col>
