428df582878136e622c58f7d30d72aa84a89eee5
Murph Grainger <murph.grainger@gmail.com>
2017-06-21 15:36:09 -0600

    updated alumni card to inverse

--- src/components/alumni/AlumniCard.js
+++ src/components/alumni/AlumniCard.js
@@ -8,23 +8,21 @@ class Alumni extends React.Component {
 
     render() {
         return (
-            <Col sm="12" md="12" lg='6' className="goal-block">
-              <Card>
-              <Media>
-              <Media left>
-                <Media object src={this.props.item.photo.fields.file.url} className="alumni-photo" />
-              </Media>
-              <Media body className="alumni-body">
-                <Media heading> {this.props.item.name}</Media>
-                  <p className="subtitle">{this.props.item.currentCompany} | Denver, CO</p>
-                  <p className="subtitle">{this.props.item.cohort}</p>
-                  <p className="story">{this.props.item.story}</p>
-                  </Media>
-                </Media>
-                <CardFooter className="alumni-footer"><a className="contact-me-button" href={'mailto:' + this.props.item.email}><Button color="primary">Contact Me</Button></a>
-                <a className="contact-me-button" target='_blank' href={this.props.item.github}><Button color="primary">See My Projects</Button></a></CardFooter>
+          <Col sm="12" md="12" lg='6' className="goal-block">
+                <Card className="alumni-card">
+                  <div className="image-holder">
+                    <img src={this.props.item.photo.fields.file.url}/>
+                  </div>
+                  <div className="alumni-body">
+                    <p className="title">{this.props.item.name}</p>
+                    <p className="subtitle">{this.props.item.currentCompany} | Denver, CO</p>
+                    <p className="subtitle">{this.props.item.cohort}</p>
+                    <p className="story">{this.props.item.story}</p>
+                  </div>
                 </Card>
               </Col>
+
+
         );
     }
 }
