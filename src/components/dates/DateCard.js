9fcba096f227aeb9c538e5c3cb8531045ea33e2a
Murph Grainger <murph.grainger@gmail.com>
2017-06-27 15:37:54 -0600

    added campus column

--- src/components/dates/DateCard.js
+++ src/components/dates/DateCard.js
@@ -16,7 +16,7 @@ class GoalCard extends React.Component {
           <Card className="date-card">
             <Col xs="12" sm="12" md="8">
             <h1>{this.props.item.startMonth} <span className="day">{this.props.item.startDate}</span> - {this.props.item.endMonth} <span className="day">{this.props.item.endDate}</span></h1>
-            <p className="date-info"><span className="campus">{this.props.item.campus}</span> | Mon-Fri: 9am-5pm</p>
+            <p className="date-info"><span className="campus-name">{this.props.item.campus}</span> | Mon-Fri: 9am-5pm</p>
             </Col>
             <Col xs="12" sm='12' md="4">
               <Button color="primary">Start Application</Button>
--- src/components/dates/Dates.js
+++ src/components/dates/Dates.js
@@ -60,10 +60,24 @@ renderItems() {
     render() {
         return (
           <div className="date-row">
-            <Col sm='12' md='4'>
+            <Col sm='12' md='5' lg="4">
               <h4 className="label">Visit The Campus</h4>
+              <div className="campus-block">
+                <div className="campus platte">
+                  <h3>Platte</h3>
+                  <h4 className="campus-address">1644 Platte Street</h4>
+                  <h4 className="campus-address">Denver, CO 80202</h4>
+                  <Button className="info-button">Register for Info Session</Button>
+                </div>
+                <div className="campus golden">
+                  <h3>Golden Triangle</h3>
+                  <h4 className="campus-address">1062 Delaware St</h4>
+                  <h4 className="campus-address">Denver, CO 80204</h4>
+                  <Button className="info-button">Register for Info Session</Button>
+                </div>
+              </div>
             </Col>
-            <Col sm='12' md='8'>
+            <Col sm='12' md='7' lg='8'>
               <h4 className="label">Upcoming Dates</h4>
               {this.renderItems()}
             </Col>
